e3021b4c7b18ae17c703c2e676a5f2d2982634e0
GustavoBauny <gustavounipam1504@gmail.com>
2022-06-23 17:18:46 -0300

    Adicionando Estruturas de Controle

---
+++ CursoUdemy/EstruturasDeControle/EstruturaSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturasDeControle
+{
+    class EstruturaSwitch
+    {
+        public static void Executar() {
+            Console.Write("Avalie meu atendimento com uma nota de 1 a 5: ");
+            int.TryParse(Console.ReadLine(), out int nota);
+
+            switch (nota) {
+                case 0:
+                    Console.WriteLine("Péssimo");
+                    Console.WriteLine("Obrigado por responder!");
+                    break;
+                case 1:
+                case 2:
+                    Console.WriteLine("Ruim");
+                    Console.WriteLine("Obrigado por responder!");
+                    break;
+                case 3:
+                    Console.WriteLine("Regular");
+                    Console.WriteLine("Obrigado por responder!");
+                    break;
+                case 4:
+                    Console.WriteLine("Bom");
+                    Console.WriteLine("Obrigado por responder!");
+                    break;
+                case 5:
+                    Console.WriteLine("Ótimo");
+                    Console.WriteLine("Obrigado por responder!");
+                    break;
+                default:
+                    Console.WriteLine("Nota inválida");
+                    Console.WriteLine("Favor responder uma nota válida!");
+                    break;
+            }
+
+        }
+    }
+}
